ecbbf5cf50b987bb32e8cab60f526c6c02768b23
Ogacha <ogacha@gmail.com>
2012-12-31 02:16:52 +0900

    シンプル版、一応完成。右クリックを活用するインストーラーが、まだない。

---
+++ TEST_ファイル屋/FileExTest.cs
@@ -0,0 +1,89 @@
+﻿using 尾画茶;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TEST_ファイル屋
+{
+    
+    
+    /// <summary>
+    ///FileExTest のテスト クラスです。すべての
+    ///FileExTest 単体テストをここに含めます
+    ///</summary>
+    [TestClass()]
+    public class FileExTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        // 
+        //テストを作成するときに、次の追加属性を使用することができます:
+        //
+        //クラスの最初のテストを実行する前にコードを実行するには、ClassInitialize を使用
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //クラスのすべてのテストを実行した後にコードを実行するには、ClassCleanup を使用
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //各テストを実行する前にコードを実行するには、TestInitialize を使用
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //各テストを実行した後にコードを実行するには、TestCleanup を使用
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///全ファイル取得 のテスト
+        ///</summary>
+        [TestMethod()]
+        public void 全ファイル取得Test()
+        {
+            if(Directory.Exists(@"C:\PathTest")) Directory.Delete(@"C:\PathTest",true);
+            Directory.CreateDirectory(@"C:\PathTest");
+            Directory.CreateDirectory(@"C:\PathTest\Dir1");
+            Directory.CreateDirectory(@"C:\PathTest\Dir1\Dir2");
+            File.WriteAllText(@"C:\PathTest\一番上.txt","test");
+            File.WriteAllText(@"C:\PathTest\Dir1\二番.txt", "test");
+            File.WriteAllText(@"C:\PathTest\Dir1\Dir2\三番.txt", "test");
+            var filesAndDirectories = new[] { @"C:\PathTest\Dir1", @"C:\PathTest\一番上.txt" };
+            var expected = new[] { @"C:\PathTest\一番上.txt", @"C:\PathTest\Dir1\二番.txt", @"C:\PathTest\Dir1\Dir2\三番.txt" };
+            var actual = FileEx.全ファイル取得(filesAndDirectories).OrderBy(p=>p.Length).ToArray();
+            Assert.AreEqual(string.Join(",",expected), string.Join(",",actual));
+        }
+    }
+}
--- ZoneIDRemover/Program.cs
+++ ZoneIDRemover/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using 尾画茶;
+using System.Text;
 
 namespace ZoneIDRemover
 {
@@ -12,17 +13,39 @@ namespace ZoneIDRemover
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
-        static void Main(string[] files)
+        static void Main(string[] fileAndDirectories)
         {
-            if (files.Length == 0)
+            if (fileAndDirectories.Length == 0)
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new 設定画面());
+                MessageBox.Show("ファイルのブロックを解除するには、\r\n"
+                +"解除したいファイルやフォルダーをブロック解除くんに\r\n"
+                +"ドラッグ＆ドロップなどして、起動してください。\r\n"
+                +"複数ファイルやフォルダー一括にも対応しています。",
+                "ブロック解除くんの使い方",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            var files = FileEx.全ファイル取得(fileAndDirectories);
+            var 結果 = files.Select(file => new { ファイル名 = file, 結果 = ADS係.RemoveZoneID(file) }).ToArray();
+            if (結果.All(result => result.結果 == null))
+            {
+                MessageBox.Show("ブロックされているファイルはありませんでした。");
+                return;
+            }
+            var mes=new StringBuilder();
+            var 失敗= string.Join("\r\n", 結果.Where(result => result.結果 == false)
+                    .Select(result => result.ファイル名));
+            var 成功数=結果.Count(result=>result.結果==true);
+            if(成功数>0) mes.AppendFormat("ブロック解除にすべて成功しました。（{0}個）",成功数).AppendLine();
+            if(成功数>0 && 失敗.Any()) mes.Append("ただし、");
+            if (失敗.Any())
+            {
+                mes.AppendLine("以下のファイルのブロック解除に失敗しました。").AppendLine(失敗);
+                MessageBox.Show( mes.ToString() , "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
             else
             {
-                foreach (var file in files) ADS係.RemoveZoneID(file);
+                MessageBox.Show(mes.ToString());
             }
         }
     }
